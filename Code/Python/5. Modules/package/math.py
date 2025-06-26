af60d50e7ba94c8f5706a30187df4e7136a6fbdf
Mukesh Kumar Patel <mukeshkumarpatel142@gmail.com>
2025-06-26 22:58:47 +0530

    module&packages

---
+++ Code/Python/5. Modules/package/math.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+    return a+b
+
+def subs(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
---
+++ Code/Python/5. Modules/test.py
@@ -0,0 +1,6 @@
+from package.math import *
+from package.subpackages.modulus import mod
+print(f"Addition is {addition(23,10)}")
+print(f"Multiplication is {multiply(23,10)}")
+
+print(f"This is from subpackage Modulus is: {mod(23,3)}")
